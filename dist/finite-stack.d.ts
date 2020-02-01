1bc4b60321fd985ddce121a46b9427f03e01b536
Bikossor <andre.lichtenthaeler@gmail.com>
2020-02-01 17:58:18 +0100

    Closes #7

--- dist/finite-stack.d.ts
+++ dist/finite-stack.d.ts
@@ -1,10 +1,33 @@
+/**
+ * A limitable LIFO data structure
+ * @author André Lichtenthäler (Bikossor)
+ * @license GPL-3.0
+ */
 declare class FiniteStack {
-    Limit: any;
+    Limit: number;
     Stack: any[];
-    constructor(limit: any);
+    constructor(limit: number);
+    /**
+     * Adds one ore more elements to the end of the `FiniteStack`
+     * @param value The element(s) to add
+     */
     push(value: any): void;
+    /**
+     * Returns the last element from the `FiniteStack`
+     */
     peek(): any;
+    /**
+     * Returns and removes the last element from the `FiniteStack`
+     */
     pop(): any;
+    /**
+     * Empty the `FiniteStack`
+     */
     clear(): void;
-    contains(searchElement: any, fromIndex: any): boolean;
+    /**
+     * Checks if the `FiniteStack` contains a specific value
+     * @param searchElement The value to search for
+     * @param fromIndex The index at which to begin to search for
+     */
+    contains(searchElement: any, fromIndex: number): boolean;
 }
--- src/finite-stack.ts
+++ src/finite-stack.ts
@@ -1,12 +1,22 @@
+/**
+ * A limitable LIFO data structure
+ * @author André Lichtenthäler (Bikossor)
+ * @license GPL-3.0
+ */
 class FiniteStack {
-    Limit: any;
+    Limit: number;
     Stack: any[];
-    constructor(limit) {
+
+    constructor(limit: number) {
         this.Limit = limit;
         this.Stack = new Array();
     }
 
-    push(value) {
+    /**
+     * Adds one ore more elements to the end of the `FiniteStack`
+     * @param value The element(s) to add
+     */
+    push(value: any): void {
         if (this.Stack.length >= this.Limit) {
             this.Stack.shift();
         }
@@ -14,19 +24,33 @@ class FiniteStack {
         this.Stack.push(value);
     }
 
-    peek() {
+    /**
+     * Returns the last element from the `FiniteStack`
+     */
+    peek(): any {
         return this.Stack[this.Stack.length - 1];
     }
 
-    pop() {
+    /**
+     * Returns and removes the last element from the `FiniteStack`
+     */
+    pop(): any {
         return this.Stack.pop();
     }
 
-    clear() {
+    /**
+     * Empty the `FiniteStack`
+     */
+    clear(): void {
         this.Stack = new Array();
     }
 
-    contains(searchElement, fromIndex) {
+    /**
+     * Checks if the `FiniteStack` contains a specific value
+     * @param searchElement The value to search for
+     * @param fromIndex The index at which to begin to search for
+     */
+    contains(searchElement: any, fromIndex: number): boolean {
         return this.Stack.indexOf(searchElement, fromIndex) > -1;
     }
 }
